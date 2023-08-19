a67f49a07069c0077cc4edc4bedba69a924136c8
Bruce Moura <brucemoura@outlook.com>
2023-08-19 15:01:50 -0300

    otimizado para a produção

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//ativando links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(ativarLink)
+
+
+//Ativar itens do orçamento
+const parametro = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro)
+    if (elemento) {
+        elemento.checked = true
+    }
+    console.log(elemento)
+}
+
+parametro.forEach(ativarProduto)
+
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle('ativa')
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+    console.log(ativa)
+}
+
+function eventosPerguntas(perguntas) {
+    perguntas.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
+
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+//animação
+//usando o if (window.nome-do-plugin) so vai ocorrer se o simpleAnime existir, se ele não existir NÃO vai da erro para mudar de pagina, ou seja ele so vai existir nas paginas que ele foi ativado
+if (window.SimpleAnime) {
+    new SimpleAnime();
+};
